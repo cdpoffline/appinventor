448ab4661cb6061ef6db4b3e9824174d0500786b
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-03-07 16:22:01 -0800

    running appinventor not as root

--- bin/startup.sh
+++ bin/startup.sh
@@ -8,11 +8,18 @@ mkdir -p "$log_folder"
 appengine_log="`realpath \"$log_folder/appengine.log\"`"
 buildserver_log="`realpath \"$log_folder/buildserver.log\"`"
 
-(
+user="`whoami`"
+if [ -n "$1" ]
+then
+  user="$1"
+fi
+echo "user $user"
+
+sudo -u "$user" (
   ./appengine-java-sdk/bin/dev_appserver.sh --port=8888 --address=0.0.0.0 ./appinventor-sources/appinventor/appengine/build/war/ 1>>"$appengine_log" 2>>"$appengine_log"
 ) &
 
-(
+sudo -u "$user" (
   cd appinventor-sources/appinventor/buildserver
   ant RunLocalBuildServer 1>>"$buildserver_log" 2>>"$buildserver_log"
 ) &
